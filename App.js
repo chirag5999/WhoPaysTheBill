6d8084f151cc7917edaf9d612ccb96e37f2af1e2
chirag5999 <125373391+chirag5999@users.noreply.github.com>
2023-08-13 22:38:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React,{useContext} from "react";
+import Stage1 from "./components/stage1";
+import Stage2 from "./components/stage2";
+import { MyContext } from "./context";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './style/app.css'
+
+const App = () => {
+
+  const context = useContext(MyContext)
+
+
+  return(
+    <div className="wrapper">
+        <div className="center-wrapper">
+          <h1>Who pays the bill?</h1>
+          {
+            context.state.stage === 1 ?
+            <Stage1/>
+            :
+            <Stage2/>
+          }
+        </div>
+    </div>
+  )
+
+}
+
+export default App;
